bdfcad82685ec1073f875450c04deac75061298f
Nicolás Navarro Rincón <nicolas@myseocompany.co>
2025-06-29 21:09:45 -0500

    Fix agreement filter query

--- app/Filament/HospitalAdmin/Clusters/Patients/Resources/RipsPatientServiceResource.php
+++ app/Filament/HospitalAdmin/Clusters/Patients/Resources/RipsPatientServiceResource.php
@@ -102,8 +102,9 @@ public static function table(Table $table): Table
                 ->label('Convenio')
                 ->options(RipsTenantPayerAgreement::pluck('name', 'id'))
                 ->query(function (Builder $query, $state) {
-                    $query->whereHas('billingDocument', function (Builder $subQuery) use ($state) {
-                        $subQuery->where('agreement_id', $state['value']);
+                    $value = is_array($state) ? ($state['value'] ?? null) : $state;
+                    $query->whereHas('billingDocument', function (Builder $subQuery) use ($value) {
+                        $subQuery->where('agreement_id', $value);
                     });
                 }),
             Filter::make('document_number')
