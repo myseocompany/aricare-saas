5ba4ae9bc3b32a6dcc9d5ab97f56b903b973db02
Nicolás Navarro Rincón <nicolas@myseocompany.co>
2025-06-29 20:53:46 -0500

    Add patient service filters

--- app/Filament/HospitalAdmin/Clusters/Patients/Resources/RipsPatientServiceResource.php
+++ app/Filament/HospitalAdmin/Clusters/Patients/Resources/RipsPatientServiceResource.php
@@ -6,8 +6,15 @@ use App\Filament\HospitalAdmin\Clusters\Patients;
 use App\Filament\HospitalAdmin\Clusters\Patients\Resources\RipsPatientServiceResource\Pages;
 use App\Filament\HospitalAdmin\Clusters\Patients\Resources\RipsPatientServiceResource\RelationManagers;
 use App\Models\Rips\RipsPatientService;
+use App\Models\Patient;
+use App\Models\Rips\RipsTenantPayerAgreement;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Filters\Filter;
+use Malzariey\FilamentDaterangepickerFilter\Filters\DateRangeFilter;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -89,7 +96,40 @@ public static function table(Table $table): Table
                 ->label('Fecha de Actualización'),
         ])
         ->filters([
-            // Agregar filtros si es necesario
+            DateRangeFilter::make('service_datetime')
+                ->label('Fecha de Servicio'),
+            SelectFilter::make('agreement_id')
+                ->label('Convenio')
+                ->options(RipsTenantPayerAgreement::pluck('name', 'id'))
+                ->query(function (Builder $query, $value) {
+                    $query->whereHas('billingDocument', function (Builder $subQuery) use ($value) {
+                        $subQuery->where('agreement_id', $value);
+                    });
+                }),
+            Filter::make('document_number')
+                ->form([
+                    TextInput::make('document_number')
+                        ->label('Número de Factura'),
+                ])
+                ->query(function (Builder $query, array $data) {
+                    $query->when($data['document_number'], function (Builder $query, $value) {
+                        $query->whereHas('billingDocument', function (Builder $subQuery) use ($value) {
+                            $subQuery->where('document_number', 'like', "%{$value}%");
+                        });
+                    });
+                }),
+            Filter::make('patient_id')
+                ->form([
+                    Select::make('patient_id')
+                        ->label('Paciente')
+                        ->searchable()
+                        ->options(Patient::getActivePatientNames()->toArray()),
+                ])
+                ->query(function (Builder $query, array $data) {
+                    $query->when($data['patient_id'], function (Builder $query, $value) {
+                        $query->where('patient_id', $value);
+                    });
+                }),
         ])
         ->actions([
             Tables\Actions\ViewAction::make(),
