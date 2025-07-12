58e97614b3e88a66134cc3b83521361351174222
myseocompany <nicolas@myseocompany.co>
2025-07-12 09:18:03 -0500

    migra si no existen rips_patient_service_consultation_diagnoses', 'rips_diagnosis_type_id

--- database/migrations/2025_05_25_081545_add_consultation_type_to_rips_consultation_diagnoses.php
+++ database/migrations/2025_05_25_081545_add_consultation_type_to_rips_consultation_diagnoses.php
@@ -11,17 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('rips_patient_service_consultation_diagnoses', function (Blueprint $table) {
-            $table->unsignedBigInteger('rips_diagnosis_type_id')
-                ->nullable()
-                ->after('rips_patient_service_consultation_id');
+        if (!Schema::hasColumn('rips_patient_service_consultation_diagnoses', 'rips_diagnosis_type_id')) {
+            Schema::table('rips_patient_service_consultation_diagnoses', function (Blueprint $table) {
+                $table->unsignedBigInteger('rips_diagnosis_type_id')
+                    ->nullable()
+                    ->after('rips_patient_service_consultation_id');
 
-            $table->foreign('rips_diagnosis_type_id', 'rpscd_diagnosis_type_fk')
-                ->references('id')
-                ->on('rips_diagnosis_types')
-                ->nullOnDelete();
-        });
-
+                $table->foreign('rips_diagnosis_type_id', 'rpscd_diagnosis_type_fk')
+                    ->references('id')
+                    ->on('rips_diagnosis_types')
+                    ->nullOnDelete();
+            });
+        }
     }
 
     /**
