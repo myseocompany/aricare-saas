03444313fef5e61c7ce26ac620c517f973b5b616
myseocompany <nicolas@myseocompany.co>
2025-08-22 13:31:02 -0500

    Google Ads

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -1,1 +1,11 @@
 <script src="{{ asset('js/jquery.min.js') }}" type="text/javascript"></script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17489606467">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-17489606467');
+</script>
